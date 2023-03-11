58ab00af5e3571b646e639687afb3d1f8c12855c
FaezehBehroozi <talenaz79@gmail.com>
2023-03-12 01:02:05 +0330

    Added JavaScript (API)

--- src/app.js
+++ src/app.js
@@ -1,21 +1,44 @@
-let currentDate = new Date();
-let time = document.querySelector("h6");
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[currentDate.getDay()];
-let hour = currentDate.getHours();
-let min = currentDate.getMinutes();
-if (hour < 10) {
-  hour = `0${hour}`;
+function formatDate(timestamp) {
+  let currentDate = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[currentDate.getDay()];
+  let hour = currentDate.getHours();
+  let minute = currentDate.getMinutes();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  if (minute < 10) {
+    minute = `0${min}`;
+  }
+  return `${day} ${hour}:${minute}`;
 }
-if (min < 10) {
-  min = `0${min}`;
+
+function showTemperature(response) {
+  let city = document.querySelector("#city-name");
+  let temperature = document.querySelector("#temperature");
+  let date = document.querySelector("#date");
+  let description = document.querySelector("#description");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
+  city.innerHTML = response.data.name;
+  temperature.innerHTML = Math.round(response.data.main.temp);
+  date.innerHTML = formatDate(response.data.dt * 1000);
+  description.innerHTML = response.data.weather[0].description;
+  humidity.innerHTML = response.data.main.humidity;
+  wind.innerHTML = Math.round(response.data.wind.speed);
 }
-time.innerHTML = `${day} ${hour}:${min}`;
+
+let units = "metric";
+let city = "Venice";
+let apiKey = "2a2eaa51d996796495bf456e5b58adf4";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+axios.get(apiUrl).then(showTemperature);
