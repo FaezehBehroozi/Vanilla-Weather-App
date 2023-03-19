99a0a76dc4929280897c15dccee14ca375c3a3ce
FaezehBehroozi <talenaz79@gmail.com>
2023-03-19 15:48:52 +0330

    Displayed the forecast data by using API

--- src/app.js
+++ src/app.js
@@ -21,25 +21,36 @@ function formatDate(timestamp) {
   return `${day} ${hour}:${minute}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = date.getDay();
+  return days[day];
+}
+
 function displayForecast(response) {
+  let dailyForecast = response.data.daily;
   let forecast = document.querySelector("#forecast");
-  let days = ["Fri", "Sat", "Sun", "Mon", "Tue", "Wed"];
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  dailyForecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
     <div class="col-2">
       <div>
-        ${day}
+        ${formatDay(forecastDay.dt)}
       </div>
-      <img src="image/rainy.png" alt="sunny" class="future" />
+      <img src="https://openweathermap.org/img/wn/${
+        forecastDay.weather[0].icon
+      }@2x.png" alt="sunny" class="future" />
       <div>
-        <span class="high"> 14° </span> 
-        <span class="low"> 8° </span>
+        <span class="max"> ${Math.round(forecastDay.temp.max)}° </span> 
+        <span class="min"> ${Math.round(forecastDay.temp.min)}° </span>
       </div>
     </div>
   `;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecast.innerHTML = forecastHTML;
